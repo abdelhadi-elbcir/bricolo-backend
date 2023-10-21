7e37797da138f8ff71bc42b1573ffd022a3e0947
sara <sarao.beyad@gmail.com>
2023-10-21 14:34:24 +0100

    adding lambok

--- src/main/java/com/securedapp/springjwt/models/City.java
+++ src/main/java/com/securedapp/springjwt/models/City.java
@@ -3,7 +3,13 @@ package com.securedapp.springjwt.models;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 
+@Component
+@Getter
+@Setter
 public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
