7c8b93a895670c7244c507eacce52e4c0f1bd406
abdelhadi-22 <abdoelbcir33@gmail.com>
2023-11-13 11:48:03 +0100

    implementing serviceImpl

--- src/main/java/com/securedapp/springjwt/services/impl/ServiceServiceImpl.java
+++ src/main/java/com/securedapp/springjwt/services/impl/ServiceServiceImpl.java
@@ -2,10 +2,12 @@ package com.securedapp.springjwt.services.impl;
 
 import com.securedapp.springjwt.dto.ServiceDto;
 import com.securedapp.springjwt.mappers.ServiceMapper;
+import com.securedapp.springjwt.models.Service;
 import com.securedapp.springjwt.repository.ServiceRepository;
 import com.securedapp.springjwt.services.facade.ServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceServiceImpl implements ServiceService {
@@ -26,21 +28,44 @@ public class ServiceServiceImpl implements ServiceService {
 
     @Override
     public ServiceDto update(ServiceDto serviceDto, Long id) {
-        return null;
+        Service service = serviceRepository.findById(id).orElse(null);
+        if(service != null){
+            service.setCategory(serviceDto.getCategory());
+            service.setCity(serviceDto.getCity());
+            service.setAnnounceList(serviceDto.getAnnounceList());
+            service.setDescription(serviceDto.getDescription());
+            service.setImage(serviceDto.getImage());
+            service.setPhone(serviceDto.getPhone());
+            service.setPrice(serviceDto.getPrice());
+            service.setTitle(serviceDto.getTitle());
+            service.setUser(serviceDto.getUser());
+        }
+        return  null;
     }
 
     @Override
     public String delete(Long id) {
-        return null;
+        if(serviceRepository.findById(id).isPresent()){
+            serviceRepository.deleteById(id);
+            return "Service deleted !";
+        }
+        return  "Sevice Doesn't exist !";
     }
 
     @Override
     public ServiceDto getItem(Long id) {
-        return null;
+        if(serviceRepository.findById(id).isPresent())
+            return serviceMapper.toDto(serviceRepository.findById(id).get());
+        return  null;
     }
 
     @Override
     public List<ServiceDto> getList() {
-        return null;
+        List<Service> serviceList = serviceRepository.findAll();
+        List<ServiceDto> serviceDtoList = new ArrayList<>();
+        for (Service service : serviceList)
+            if(service != null)
+                serviceDtoList.add(serviceMapper.toDto(service));
+        return  serviceDtoList;
     }
 }
